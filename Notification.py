6f587143d666d656e3780efd74ca6361f5277684
Oleksandr Shvaienko <shvayenkoalex@gmail.com>
2022-12-16 22:00:39 +0200

    Creating class for data storing and creating bot functions.

--- Notification.py
+++ Notification.py
@@ -1,73 +1,28 @@
 from datetime import datetime
 import telebot
-from TOKEN import token, chatId
-from notifiers import get_notifier
+from TOKEN import token
+from telebot import types
+from Data import Dicts
+
 bot = telebot.TeleBot(token)
-@bot.message_handler(content_types=['text'])
-def get_text_messages(message):
-    if message.text == '/start':
-        bot.send_message(message.from_user.id, "Привіт!\nЦей бот створено для моніторингу графіка відключень світла у місті Львів. "
-                                               "Також є можливість налаштувати сповіщення про відлючення світла відповідно до певної групи.\n"
-                                               "Щоб продовжити натисніть /putin_huilo")
-    elif message.text == '/putin_huilo':
-        bot.send_message(message.from_user.id, 'Оберіть номер вашої групи:')
-
-
 class Electricity:
     current_time = datetime.now().strftime("%H")
 
-    TYPE_1 = {'1-5': 'Можливе Відключення',
-              '5-9': 'Немає Енергії',
-              '9-13': 'Є Енергія',
-              '13-17': 'Можливе Відключення',
-              '17-21': 'Немає Енергії',
-              '21-1': 'Є Енергія'}
+    GROUP_1 = Dicts.GROUP_1
+    GROUP_2 = Dicts.GROUP_2
+    GROUP_3 = Dicts.GROUP_3
 
-    TYPE_2 = {'1-5': 'Немає Енергії',
-              '5-9': 'Є Світло',
-              '9-13': 'Можливе Відкючення',
-              '13-17': 'Немає Енергії',
-              '17-21': 'Є Енергія',
-              '21-1': 'Можливе Відключення'}
-
-    TYPE_3 = {'1-5': 'Є Енергія',
-              '5-9': 'Можливе Відключення',
-              '9-13': 'Немає Енергії',
-              '13-17': 'Є Енергія',
-              '17-21': 'Можливе Відключення',
-              '21-1': 'Немає Енергії'}
-
-    GROUP_1 = {'Понеділок': TYPE_2,
-               'Вівторок': TYPE_3,
-               'Середа': TYPE_1,
-               'Четвер': TYPE_2,
-               "П'ятниця": TYPE_3,
-               'Субота': TYPE_1,
-               'Неділя': TYPE_2}
-
-    GROUP_2 = {'Понеділок': TYPE_3,
-               'Вівторок': TYPE_1,
-               'Середа': TYPE_2,
-               'Четвер': TYPE_3,
-               "П'ятниця": TYPE_1,
-               'Субота': TYPE_2,
-               'Неділя': TYPE_3}
-
-    GROUP_3 = {'Понеділок': TYPE_1,
-               'Вівторок': TYPE_2,
-               'Середа': TYPE_3,
-               'Четвер': TYPE_1,
-               "П'ятниця": TYPE_2,
-               'Субота': TYPE_3,
-               'Неділя': TYPE_1}
-
-    def __init__(self, group, day, time=current_time):
+    def __init__(self, group, day=None, time=current_time):
         self.group = group
         self.day = day
         self.time = time
 
     def get_time_zone(self):
-        return '-'.join(list(filter(lambda x: int(self.current_time) in range(int(x[0]), int(x[1])), map(lambda x: x.split('-'), self.TYPE_1.keys())))[0])
+        if int(self.time) >= 21:
+            return '21-1'
+        else:
+            return '-'.join(list(filter(lambda x: int(self.current_time) in range(int(x[0]), int(x[1])),
+                                        map(lambda x: x.split('-'), Dicts.TYPE_1.keys())))[0])
 
     def inform(self):
         pass
@@ -88,7 +43,48 @@ class Electricity:
             return self.GROUP_3[self.day]
 
 
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id,"Привіт!\nЦей бот створено для моніторингу графіка відключень світла у місті Львів. "
+                                    "Також є можливість налаштувати сповіщення про відлючення світла відповідно до певної групи.\n"
+                                    "Щоб продовжити натисніть /putin_huilo")
+
+
+@bot.message_handler(commands=['putin_huilo'])
+def choose_group(message):
+    markup_inline = types.InlineKeyboardMarkup()
+    group_1 = types.InlineKeyboardButton(text='Перша група', callback_data=1)
+    group_2 = types.InlineKeyboardButton(text='Друга група', callback_data=2)
+    group_3 = types.InlineKeyboardButton(text='Третя група', callback_data=3)
+    markup_inline.add(group_1, group_2, group_3)
+    bot.send_message(message.chat.id, 'Оберіть номер вашої групи:', reply_markup=markup_inline)
+
+@bot.callback_query_handler(func= lambda call: True)
+def choose_day(call):
+    if call.data in ('1', '2', '3'):
+        new = Electricity(call.data)
+        markup_inline_2 = types.InlineKeyboardMarkup()
+        monday = types.InlineKeyboardButton(text='Понеділок', callback_data='Понеділок')
+        tuesday = types.InlineKeyboardButton(text='Вівторок', callback_data='Вівторок')
+        wednesday = types.InlineKeyboardButton(text='Середа', callback_data='Середа')
+        thursday = types.InlineKeyboardButton(text='Четвер', callback_data='Четвер')
+        friday = types.InlineKeyboardButton(text="П'ятниця", callback_data="П'ятниця")
+        saturday = types.InlineKeyboardButton(text="Субота", callback_data="Субота")
+        sunday = types.InlineKeyboardButton(text="Неділя", callback_data="Неділя")
+        markup_inline_2.add(monday, tuesday, wednesday, thursday, friday, saturday, sunday)
+        bot.send_message(call.message.chat.id, 'Оберіть день:', reply_markup=markup_inline_2)
+
+@bot.callback_query_handler(func= lambda call: True)
+def print_schedule(call):
+    print(call)
+    bot.send_message(call.message.chat.id, str(Electricity(call).get_day_sсhedule()))
+
+
+
 
 
 bot.polling(none_stop=True, interval=0)
+
+a = Electricity(1, 'Понеділок')
+print(a.get_condition())
 
